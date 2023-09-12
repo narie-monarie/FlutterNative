460ec24028511c145a16614effe812617057f944
job monari <jobmonari7@gmail.com>
2023-09-12 12:42:11 +0300

    just pushing some code

--- myapplication/lib/pages/home_page.dart
+++ myapplication/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//Stateless are Immutable while Stateful are mutable
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -7,7 +8,65 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter Map"),
+        title: const Text("FLUTTER MAP"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Image.asset('images/cherry-916.png'),
+                    const Text(
+                      "Space Weather",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Text("Description")
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      height: 170,
+                      child: Image.asset('images/cherry-149.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      height: 170,
+                      child: Image.asset('images/cherry-703.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                width: double.infinity,
+                height: 300.0,
+                child: Image.asset('images/cherry-79.png'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
